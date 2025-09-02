d0c8f97296c8effc43d31c8771cb447888aca306
antany <antany84@gmail.com>
2025-09-02 17:38:23 -0400

    until coverage

---
+++ tests/App.test.jsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import App from '../src/App'
+
+describe('App', () => {
+  it('renders HelloWorld', () => {
+    render(<App />)
+    expect(screen.getByText('HelloWorld')).toBeInTheDocument()
+  })
+})
+
